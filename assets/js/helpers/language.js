e21cda966315dad5e19b0737261c97ed901744aa
Enrique N. <116834301+Enrikluna83@users.noreply.github.com>
2024-12-13 15:02:53 -0600

    Again!

---
+++ assets/js/helpers/language.js
@@ -0,0 +1,15 @@
+const langToggleDOM = document.getElementById('language-toggle');
+const currentLang = window.location.href.includes('index-en.html');
+langToggleDOM.checked = currentLang;
+
+function chageLang() {
+    langToggleDOM.addEventListener('change', () => {
+        if (langToggleDOM.checked) {
+            window.location.href = 'index-en.html';
+        } else {
+            window.location.href = 'index.html';
+        }
+    });
+}
+
+export default chageLang;
